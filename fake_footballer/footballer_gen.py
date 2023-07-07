a5987ff6a9372696f519f5b6c6b68757e28831da
Marek Zarzycki <ma.zarzycki@yahoo.com>
2023-07-07 22:08:05 +0200

    Execution of the script removed from the main file

--- fake_footballer/footballer_gen.py
+++ fake_footballer/footballer_gen.py
@@ -1,7 +1,7 @@
 import datetime
 import pandas as pd
 import random
-from data import ENG, ITA, SPA, GER, position
+from fake_footballer.data import ENG, ITA, SPA, GER, position
 
 class FootballerGenerator:
     def __init__(self, first_names, last_names, cities, club, position):
@@ -44,64 +44,4 @@ class FootballerGenerator:
             'goals': goals,
             'assists': assists
         }
-
-# Map the user's choice to the country code
-country_mapping = {
-    '1': 'ENG',
-    '2': 'ITA',
-    '3': 'SPA',
-    '4': 'GER'
-}
-
-datasets = []  # List to store generated datasets
-
-while True:
-    print("Available countries:")
-    print("1. England (ENG)")
-    print("2. Italy (ITA)")
-    print("3. Spain (SPA)")
-    print("4. Germany (GER)")
-
-    country_choice = input("Enter the number corresponding to the desired country.\nLater you will be asked the same question once again if you want to create a data set with players from different countries (or 'q' to quit): ")
-
-    if country_choice.lower() == 'q':
-        break  # Exit the loop if user enters 'q'
-
-    # Validate the user's choice
-    if country_choice not in country_mapping:
-        print("Invalid country choice. Please try again.")
-        continue  # Continue to the next iteration of the loop
-
-    country = country_mapping[country_choice]
-
-    # Set up the FootballerGenerator based on the chosen country
-    if country == 'ENG':
-        footballer_generator = FootballerGenerator(ENG[0], ENG[1], ENG[2], ENG[3], position)
-    elif country == 'ITA':
-        footballer_generator = FootballerGenerator(ITA[0], ITA[1], ITA[2], ITA[3], position)
-    elif country == 'SPA':
-        footballer_generator = FootballerGenerator(SPA[0], SPA[1], SPA[2], SPA[3], position)
-    elif country == 'GER':
-        footballer_generator = FootballerGenerator(GER[0], GER[1], GER[2], GER[3], position)
-
-    # Prompt the user to enter the number of fake footballers to generate
-    num_footballers = input("Enter the number of fake footballers to generate: ")
-
-    # Validate the user's input for the number of footballers
-    if not num_footballers.isdigit() or int(num_footballers) <= 0:
-        print("Invalid number of footballers. Please try again.")
-        continue  # Continue to the next iteration of the loop
-
-    num_footballers = int(num_footballers)
-
-    # Generate fake footballers
-    fake_footballers = [footballer_generator.generate_fake_footballer(country) for _ in range(num_footballers)]
-
-    datasets.append(pd.DataFrame(fake_footballers))  # Add the generated dataset to the list
-
-# Concatenate and save all datasets to a single Excel file
-filename = 'fake_footballers.xlsx'
-final_dataset = pd.concat(datasets)
-final_dataset.to_excel(filename, index=False)
-print(f"Fake footballers data saved to {filename}:\n")
-print(final_dataset)
+    # TODO add a piece of code that allows you to create one fake club with 23 players

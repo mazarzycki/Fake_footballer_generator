30aec7142ffff320146561835331793021d8cb5f
Marek Zarzycki <ma.zarzycki@yahoo.com>
2023-07-07 15:51:44 +0200

    Create players from various countries added

--- fake_footballer/footballer_gen.py
+++ fake_footballer/footballer_gen.py
@@ -1,7 +1,7 @@
 import datetime
 import pandas as pd
 import random
-from fake_footballer.data import ENG, ITA, SPA, GER, position
+from data import ENG, ITA, SPA, GER, position
 
 class FootballerGenerator:
     def __init__(self, first_names, last_names, cities, club, position):
@@ -29,7 +29,7 @@ class FootballerGenerator:
         # TODO Make goals and assists more realistic
         # TODO Add more statistics: xG and others
         goals = (lambda pos: 0 if pos == "GK" else random.randint(0, min(minutes_played // 50, 6)) if pos in ["LB", "CB", "RB"] else random.randint(0, min(minutes_played // 50, 9)) if pos in ["DM", "CM"] else random.randint(0, min(minutes_played // 50, 12)) if pos in ["AM", "LM", "RM"] else random.randint(0, min(minutes_played // 30, 28)))(position)
-        assists = (lambda pos: random.randint(0, min(minutes_played // 50, 3)) if pos == "GK" else random.randint(0, min(minutes_played // 50, 6)) if pos in ["LB", "CB", "RB"] else random.randint(0, min(minutes_played // 50, 9)) if pos in ["DM", "CM"] else random.randint(0, min(minutes_played // 50, 12)) if pos in ["AM", "LM", "RM"] else random.randint(0, min(minutes_played // 30, 19)))(position)
+        assists = (lambda pos: random.randint(0, min(minutes_played // 50, 3)) if pos == "GK" else random.randint(0, min(minutes_played // 50, 6)) if pos in ["LB", "CB", "RB"] else random.randint(0, min(minutes_played // 50, 9)) if pos in ["DM", "CM"] else random.randint(0, min(minutes_played // 50, 12)) if pos in ["AM", "LM", "RM"] else random.randint(0, min(minutes_played // 50, 19)))(position)
 
 
         return {
@@ -45,17 +45,6 @@ class FootballerGenerator:
             'assists': assists
         }
 
-# Prompt the user to choose the country
-# TODO Make it possible to choose players from all countries at the same time
-print("Available countries:")
-print("1. England (ENG)")
-print("2. Italy (ITA)")
-print("3. Spain (SPA)")
-print("4. Germany (GER)")
-
-
-country_choice = input("Enter the number corresponding to the desired country: ")
-
 # Map the user's choice to the country code
 country_mapping = {
     '1': 'ENG',
@@ -64,42 +53,55 @@ country_mapping = {
     '4': 'GER'
 }
 
-# Validate the user's choice
-if country_choice not in country_mapping:
-    print("Invalid country choice. Exiting...")
-    exit()
+datasets = []  # List to store generated datasets
 
-country = country_mapping[country_choice]
+while True:
+    print("Available countries:")
+    print("1. England (ENG)")
+    print("2. Italy (ITA)")
+    print("3. Spain (SPA)")
+    print("4. Germany (GER)")
 
-# Set up the FootballerGenerator based on the chosen country
-if country == 'ENG':
-    footballer_generator = FootballerGenerator(ENG[0], ENG[1], ENG[2], ENG[3], position)
-elif country == 'ITA':
-    footballer_generator = FootballerGenerator(ITA[0], ITA[1], ITA[2], ITA[3], position)
-elif country == 'SPA':
-    footballer_generator = FootballerGenerator(SPA[0], SPA[1], SPA[2], SPA[3], position)
-elif country == 'GER':
-    footballer_generator = FootballerGenerator(GER[0], GER[1], GER[2], GER[3], position)
+    country_choice = input("Enter the number corresponding to the desired country.\nLater you will be asked the same question once again if you want to create a data set with players from different countries (or 'q' to quit): ")
 
-# Prompt the user to enter the number of fake footballers to generate
-num_footballers = input("Enter the number of fake footballers to generate: ")
+    if country_choice.lower() == 'q':
+        break  # Exit the loop if user enters 'q'
 
-# Validate the user's input for the number of footballers
-if not num_footballers.isdigit() or int(num_footballers) <= 0:
-    print("Invalid number of footballers. Exiting...")
-    exit()
+    # Validate the user's choice
+    if country_choice not in country_mapping:
+        print("Invalid country choice. Please try again.")
+        continue  # Continue to the next iteration of the loop
 
-num_footballers = int(num_footballers)
+    country = country_mapping[country_choice]
 
+    # Set up the FootballerGenerator based on the chosen country
+    if country == 'ENG':
+        footballer_generator = FootballerGenerator(ENG[0], ENG[1], ENG[2], ENG[3], position)
+    elif country == 'ITA':
+        footballer_generator = FootballerGenerator(ITA[0], ITA[1], ITA[2], ITA[3], position)
+    elif country == 'SPA':
+        footballer_generator = FootballerGenerator(SPA[0], SPA[1], SPA[2], SPA[3], position)
+    elif country == 'GER':
+        footballer_generator = FootballerGenerator(GER[0], GER[1], GER[2], GER[3], position)
 
-# Generate fake footballers
-fake_footballers = [footballer_generator.generate_fake_footballer(country) for _ in range(num_footballers)]
- 
-# Create DataFrame from the list of footballers
-df = pd.DataFrame(fake_footballers)
+    # Prompt the user to enter the number of fake footballers to generate
+    num_footballers = input("Enter the number of fake footballers to generate: ")
 
-# Save DataFrame to Excel file
-filename = f'fake_{country}_{num_footballers}_footballers.xlsx'
-df.to_excel(filename, index=False)
+    # Validate the user's input for the number of footballers
+    if not num_footballers.isdigit() or int(num_footballers) <= 0:
+        print("Invalid number of footballers. Please try again.")
+        continue  # Continue to the next iteration of the loop
+
+    num_footballers = int(num_footballers)
+
+    # Generate fake footballers
+    fake_footballers = [footballer_generator.generate_fake_footballer(country) for _ in range(num_footballers)]
+
+    datasets.append(pd.DataFrame(fake_footballers))  # Add the generated dataset to the list
+
+# Concatenate and save all datasets to a single Excel file
+filename = 'fake_footballers.xlsx'
+final_dataset = pd.concat(datasets)
+final_dataset.to_excel(filename, index=False)
 print(f"Fake footballers data saved to {filename}:\n")
-print(df)
+print(final_dataset)
